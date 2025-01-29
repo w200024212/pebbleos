b866164cf40ba107ee6aadb09666c70e7f3b25f1
Joshua Wise <joshua@accelerated.tech>
2025-01-29 09:59:20 +0100

    libc: fix build issues with modern GCC
    Signed-off-by: Joshua Wise <joshua@accelerated.tech>

--- src/libc/alloca.c
+++ src/libc/alloca.c
@@ -19,5 +19,8 @@
 # define __alloca(size) __builtin_alloca (size)
 
 void *alloca(size_t size) {
-  return __alloca(size);
+  /* This never worked to begin with, so let's at least make it obvious when
+   * someone calls it (while we shut up the compiler warning).  */
+  while (1);
+  /*return __alloca(size);*/
 }
