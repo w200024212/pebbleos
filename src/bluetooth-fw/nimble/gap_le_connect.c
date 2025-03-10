4fbfcde496527ab61da296941780d1fe42435a92
Liam McLoughlin <lmcloughlin@google.com>
2025-03-10 23:10:03 +0000

    bluetooth: implement bt_driver_gap_le_disconnect
    Signed-off-by: Liam McLoughlin <lmcloughlin@google.com>

--- src/bluetooth-fw/nimble/gap_le_connect.c
+++ src/bluetooth-fw/nimble/gap_le_connect.c
@@ -15,5 +15,23 @@
  */
 
 #include <bluetooth/gap_le_connect.h>
+#include <host/ble_gap.h>
 
-int bt_driver_gap_le_disconnect(const BTDeviceInternal *peer_address) { return 0; }
+#include "nimble_type_conversions.h"
+
+int bt_driver_gap_le_disconnect(const BTDeviceInternal *peer_address) {
+  uint16_t conn_handle;
+
+  if (!pebble_device_to_nimble_conn_handle(peer_address, &conn_handle)) {
+    PBL_LOG_D(LOG_DOMAIN_BT, LOG_LEVEL_ERROR,
+              "bt_driver_gap_le_disconnect: Failed to find connection handle");
+    return -1;
+  }
+
+  int rc = ble_gap_terminate(conn_handle, BLE_ERR_REM_USER_CONN_TERM);
+  if (rc != 0) {
+    PBL_LOG_D(LOG_DOMAIN_BT, LOG_LEVEL_ERROR, "ble_gap_terminate rc=%d", rc);
+  }
+
+  return rc;
+}
