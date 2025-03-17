6bb90307e73372ac8712432cf7e2d2dba7ec93b1
Gerard Marull-Paretas <gerard@teslabs.com>
2025-03-17 11:18:41 +0100

    bluetooth-fw: merge all classic stubs into single file
    Classic API will eventually be deleted, for now, merge all stub calls
    into a single place.
    
    Signed-off-by: Gerard Marull-Paretas <gerard@teslabs.com>

--- src/bluetooth-fw/nimble/bt_classic_stubs.c
+++ src/bluetooth-fw/nimble/bt_classic_stubs.c
@@ -14,7 +14,13 @@
  * limitations under the License.
  */
 
+#include <stdint.h>
+
 #include <bluetooth/classic_connect.h>
+#include <bluetooth/connectability.h>
+#include <bluetooth/features.h>
+#include <bluetooth/pairability.h>
+#include <bluetooth/reconnect.h>
 
 void bt_driver_classic_disconnect(const BTDeviceAddress* address) {}
 
@@ -25,3 +31,23 @@ bool bt_driver_classic_copy_connected_address(BTDeviceAddress* address) { return
 bool bt_driver_classic_copy_connected_device_name(char nm[BT_DEVICE_NAME_BUFFER_SIZE]) {
   return false;
 }
+
+void bt_driver_classic_update_connectability(void) {}
+
+bool bt_driver_supports_bt_classic(void) { return false; }
+
+void bt_driver_classic_pairability_set_enabled(bool enabled) {}
+
+uint32_t sys_app_comm_get_sniff_interval(void) { return 0; }
+
+void bt_driver_reconnect_pause(void) {}
+
+void bt_driver_reconnect_resume(void) {}
+
+void reconnect_set_interval(uint16_t new_interval) {}
+
+void bt_driver_reconnect_try_now(bool ignore_paused) {}
+
+void bt_driver_reconnect_reset_interval(void) {}
+
+void bt_driver_reconnect_notify_platform_bitfield(uint32_t platform_bitfield) {}
