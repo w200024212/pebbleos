d20be6061cafc529c956011b6c9d185217d5ed2e
Gerard Marull-Paretas <gerard@teslabs.com>
2025-03-17 11:36:56 +0100

    third_party/nimble: map nimble log levels to pebble
    Signed-off-by: Gerard Marull-Paretas <gerard@teslabs.com>

--- third_party/nimble/port/include/nimble/nimble_npl_os_log.h
+++ third_party/nimble/port/include/nimble/nimble_npl_os_log.h
@@ -21,13 +21,19 @@
 
 #include "system/logging.h"
 
-// TODO: This doesn't respect log levels and is generally pretty noisy...
+/* NimBLE to PebbleOS log level equivalences */
+#define NIMBLE_LOG_LEVEL_DEBUG LOG_LEVEL_DEBUG
+#define NIMBLE_LOG_LEVEL_INFO LOG_LEVEL_INFO
+#define NIMBLE_LOG_LEVEL_WARN LOG_LEVEL_WARNING
+#define NIMBLE_LOG_LEVEL_ERROR LOG_LEVEL_ERROR
+#define NIMBLE_LOG_LEVEL_CRITICAL LOG_LEVEL_ALWAYS
+
 #define BLE_NPL_LOG_IMPL(lvl)                                                        \
   static inline void _BLE_NPL_LOG_CAT(BLE_NPL_LOG_MODULE, _BLE_NPL_LOG_CAT(_, lvl))( \
       const char *fmt, ...) {                                                        \
     va_list args;                                                                    \
     va_start(args, fmt);                                                             \
-    pbl_log_vargs(LOG_LEVEL_INFO, __FILE__, __LINE__, fmt, args);                    \
+    pbl_log_vargs(NIMBLE_LOG_LEVEL_##lvl, __FILE__, __LINE__, fmt, args);            \
     va_end(args);                                                                    \
   }
 
