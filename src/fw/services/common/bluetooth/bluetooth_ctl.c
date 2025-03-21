97e781867b8a6a96406fc757aa03f917efdeaf0c
Gerard Marull-Paretas <gerard@teslabs.com>
2025-03-21 12:19:04 +0100

    fw/services/common/bluetooth: load existing bondings before starting BT
    This is needed by some stacks like NimBLE, otherwise the stack could
    find the store empty when it is started and so cached keys won't be
    loaded in time.
    
    Signed-off-by: Gerard Marull-Paretas <gerard@teslabs.com>

--- src/fw/services/common/bluetooth/bluetooth_ctl.c
+++ src/fw/services/common/bluetooth/bluetooth_ctl.c
@@ -96,12 +96,18 @@ static void prv_comm_start(void) {
   PBL_LOG(LOG_LEVEL_INFO, "BLE HRM sharing prefs: is_enabled=%u",
           config->is_hrm_supported_and_enabled);
 #endif
+#ifdef BT_REQUIRE_EARLY_BONDINGS
+  bt_persistent_storage_register_existing_ble_bondings();
+#endif
+
   s_comm_is_running = bt_driver_start(config);
   kernel_free(config);
 
   if (s_comm_is_running) {
     bt_local_addr_init();
+#ifndef BT_REQUIRE_EARLY_BONDINGS
     bt_persistent_storage_register_existing_ble_bondings();
+#endif
     gap_le_init();
     bt_local_id_configure_driver();
 #if CAPABILITY_HAS_BUILTIN_HRM
