e01de21d5eb4d7b2933af1c9507befa73d7501a8
Gerard Marull-Paretas <gerard@teslabs.com>
2025-04-03 14:02:14 +0200

    docs: add minimal Sphinx setup
    Signed-off-by: Gerard Marull-Paretas <gerard@teslabs.com>

---
+++ docs/conf.py
@@ -0,0 +1,53 @@
+# Copyright 2025 Core Devices LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "PebbleOS"
+copyright = "2025, The PebbleOS Contributors"
+author = "The PebbleOS Contributors"
+release = "0.1.0"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "myst_parser",
+]
+
+exclude_patterns = ["_build", "legacy", "Thumbs.db", ".DS_Store"]
+
+nitpicky = True
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "sphinx_book_theme"
+html_static_path = ["_static"]
+html_logo = "_static/images/logo.png"
+html_css_files = ["css/custom.css"]
+html_theme_options = {
+    "repository_url": "https://github.com/pebble-dev/pebble-firmware",
+    "repository_branch": "main",
+    "path_to_docs": "docs",
+    "use_repository_button": True,
+    "use_edit_page_button": True,
+    "use_issues_button": True,
+}
