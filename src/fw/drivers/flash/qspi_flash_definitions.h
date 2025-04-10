92d9ff48d880cd009f647ac425df2c3d0101fc88
Gerard Marull-Paretas <gerard@teslabs.com>
2025-04-10 15:40:38 +0200

    fw/drivers/flash: allow specifying read/write 'mode'
    Signed-off-by: Gerard Marull-Paretas <gerard@teslabs.com>

--- src/fw/drivers/flash/qspi_flash_definitions.h
+++ src/fw/drivers/flash/qspi_flash_definitions.h
@@ -29,9 +29,26 @@ typedef struct QSPIFlashState {
   bool fast_read_ddr_enabled;
 } QSPIFlashState;
 
+typedef enum QSPIFlashReadMode {
+  QSPI_FLASH_READ_FASTREAD,
+  QSPI_FLASH_READ_READ2O,
+  QSPI_FLASH_READ_READ2IO,
+  QSPI_FLASH_READ_READ4O,
+  QSPI_FLASH_READ_READ4IO,
+} QSPIFlashReadMode;
+
+typedef enum QSPIFlashWriteMode {
+  QSPI_FLASH_WRITE_PP,
+  QSPI_FLASH_WRITE_PP2O,
+  QSPI_FLASH_WRITE_PP4O,
+  QSPI_FLASH_WRITE_PP4IO,
+} QSPIFlashWriteMode;
+
 typedef const struct QSPIFlash {
   QSPIFlashState *state;
   QSPIPort *qspi;
   bool default_fast_read_ddr_enabled;
+  QSPIFlashReadMode read_mode;
+  QSPIFlashWriteMode write_mode;
   OutputConfig reset_gpio;
 } QSPIFlash;
