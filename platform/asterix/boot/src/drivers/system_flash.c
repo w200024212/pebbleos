f6b37e65ecb0ff12823fdd19abe0d2c73a9214b8
Gerard Marull-Paretas <gerard@teslabs.com>
2025-04-21 23:04:19 +0200

    platform/asterix/system_flash: format file
    So that CI is happy.
    
    Signed-off-by: Gerard Marull-Paretas <gerard@teslabs.com>

--- platform/asterix/boot/src/drivers/system_flash.c
+++ platform/asterix/boot/src/drivers/system_flash.c
@@ -1,17 +1,14 @@
 #include "drivers/system_flash.h"
 
+#include <hal/nrf_nvmc.h>
+
 #include "drivers/dbgserial.h"
 #include "util/misc.h"
-
-#include <hal/nrf_nvmc.h>
 
 #define FLASH_SECTOR_SIZE 0x1000
 #define FLASH_SECTOR_COUNT 256
 
-static inline bool addr_is_word_aligned(uint32_t addr)
-{
-    return (addr & 0x3u) == 0u;
-}
+static inline bool addr_is_word_aligned(uint32_t addr) { return (addr & 0x3u) == 0u; }
 
 int prv_get_sector_num_for_address(uint32_t address) {
   if (address > FLASH_SECTOR_COUNT * FLASH_SECTOR_SIZE) {
@@ -20,18 +17,16 @@ int prv_get_sector_num_for_address(uint32_t address) {
     dbgserial_putstr(" is outside system flash");
     return -1;
   }
-  for (size_t i=0; i < FLASH_SECTOR_COUNT-1; ++i) {
-    if (FLASH_SECTOR_SIZE * i <= address
-        && address < FLASH_SECTOR_SIZE * (i+1)) {
+  for (size_t i = 0; i < FLASH_SECTOR_COUNT - 1; ++i) {
+    if (FLASH_SECTOR_SIZE * i <= address && address < FLASH_SECTOR_SIZE * (i + 1)) {
       return i;
     }
   }
-  return FLASH_SECTOR_COUNT-1;
+  return FLASH_SECTOR_COUNT - 1;
 }
 
-bool system_flash_erase(
-    uint32_t address, size_t length,
-    SystemFlashProgressCb progress_callback, void *progress_context) {
+bool system_flash_erase(uint32_t address, size_t length, SystemFlashProgressCb progress_callback,
+                        void *progress_context) {
   if (length == 0) {
     // Nothing to do
     return true;
@@ -55,7 +50,6 @@ bool system_flash_erase(
 
   nrf_nvmc_mode_set(NRF_NVMC, NRF_NVMC_MODE_ERASE);
   for (int sector = first_sector; sector <= last_sector; ++sector) {
-
     nrf_nvmc_page_erase_start(NRF_NVMC, sector * FLASH_SECTOR_SIZE);
     while (!nrf_nvmc_ready_check(NRF_NVMC)) {
       // Wait for the erase to complete
@@ -68,9 +62,8 @@ bool system_flash_erase(
   return true;
 }
 
-bool system_flash_write(
-    uint32_t address, const void *data, size_t length,
-    SystemFlashProgressCb progress_callback, void *progress_context) {
+bool system_flash_write(uint32_t address, const void *data, size_t length,
+                        SystemFlashProgressCb progress_callback, void *progress_context) {
   uint32_t aligned;
   uint8_t last;
 
@@ -85,14 +78,14 @@ bool system_flash_write(
   nrf_nvmc_mode_set(NRF_NVMC, NRF_NVMC_MODE_WRITE);
 
   const uint8_t *data_array = data;
-  for (uint32_t i = 0; i < aligned; i+=4) {
+  for (uint32_t i = 0; i < aligned; i += 4) {
     while (!nrf_nvmc_ready_check(NRF_NVMC)) {
       // Wait for the write to be ready
     }
 
     nrf_nvmc_word_write(address + i, *(uint32_t *)&data_array[i]);
     if (progress_callback && i % 128 == 0) {
-      progress_callback(i/128, aligned/128, progress_context);
+      progress_callback(i / 128, aligned / 128, progress_context);
     }
   }
 
