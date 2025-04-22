e1b88983629877168dc3554874e54da1683b4b14
Gerard Marull-Paretas <gerard@teslabs.com>
2025-04-22 11:54:51 +0200

    platform/asterix/pmic: format pmic.c
    Signed-off-by: Gerard Marull-Paretas <gerard@teslabs.com>

--- platform/asterix/boot/src/drivers/pmic.c
+++ platform/asterix/boot/src/drivers/pmic.c
@@ -1,11 +1,11 @@
 #include <board.h>
-#include <nrfx_twi.h>
 #include <drivers/dbgserial.h>
+#include <nrfx_twi.h>
 #include <system/passert.h>
 
 #define LDSW_BASE 0x08U
 
-#define TASKLDSW2SET  0x02U
+#define TASKLDSW2SET 0x02U
 
 #define LDSW2LDOSEL 0x09U
 #define LDSW2LDOSEL_LDO 0x01U
@@ -15,36 +15,35 @@
 
 static nrfx_twi_t twi = NRFX_TWI_INSTANCE(BOARD_PMIC_I2C);
 static const nrfx_twi_config_t config =
-	NRFX_TWI_DEFAULT_CONFIG(BOARD_PMIC_I2C_SCL_PIN, BOARD_PMIC_I2C_SDA_PIN);
+    NRFX_TWI_DEFAULT_CONFIG(BOARD_PMIC_I2C_SCL_PIN, BOARD_PMIC_I2C_SDA_PIN);
 
-void pmic_init(void)
-{
-	nrfx_err_t err;
-	uint8_t data[3];
-	nrfx_twi_xfer_desc_t xfer = NRFX_TWI_XFER_DESC_TX(0x6b, data, sizeof(data));
+void pmic_init(void) {
+  nrfx_err_t err;
+  uint8_t data[3];
+  nrfx_twi_xfer_desc_t xfer = NRFX_TWI_XFER_DESC_TX(0x6b, data, sizeof(data));
 
-	err = nrfx_twi_init(&twi, &config, NULL, NULL);
-	PBL_ASSERT(err == NRFX_SUCCESS, "TWI init failed: %d", err);
+  err = nrfx_twi_init(&twi, &config, NULL, NULL);
+  PBL_ASSERT(err == NRFX_SUCCESS, "TWI init failed: %d", err);
 
-	nrfx_twi_enable(&twi);
+  nrfx_twi_enable(&twi);
 
-	data[0] = LDSW_BASE;
-	data[1] = LDSW2LDOSEL;
-	data[2] = LDSW2LDOSEL_LDO;
-	err = nrfx_twi_xfer(&twi, &xfer, 0);
-	PBL_ASSERT(err == NRFX_SUCCESS, "TWI transfer failed: %d", err);
+  data[0] = LDSW_BASE;
+  data[1] = LDSW2LDOSEL;
+  data[2] = LDSW2LDOSEL_LDO;
+  err = nrfx_twi_xfer(&twi, &xfer, 0);
+  PBL_ASSERT(err == NRFX_SUCCESS, "TWI transfer failed: %d", err);
 
-	data[0] = LDSW_BASE;
-	data[1] = LDSW2VOUTSEL;
-	data[2] = LDSW2VOUTSEL_1V8;
-	err = nrfx_twi_xfer(&twi, &xfer, 0);
-	PBL_ASSERT(err == NRFX_SUCCESS, "TWI transfer failed: %d", err);
+  data[0] = LDSW_BASE;
+  data[1] = LDSW2VOUTSEL;
+  data[2] = LDSW2VOUTSEL_1V8;
+  err = nrfx_twi_xfer(&twi, &xfer, 0);
+  PBL_ASSERT(err == NRFX_SUCCESS, "TWI transfer failed: %d", err);
 
-	data[0] = LDSW_BASE;
-	data[1] = TASKLDSW2SET;
-	data[2] = 0x01U;
-	err = nrfx_twi_xfer(&twi, &xfer, 0);
-	PBL_ASSERT(err == NRFX_SUCCESS, "TWI transfer failed: %d", err);
+  data[0] = LDSW_BASE;
+  data[1] = TASKLDSW2SET;
+  data[2] = 0x01U;
+  err = nrfx_twi_xfer(&twi, &xfer, 0);
+  PBL_ASSERT(err == NRFX_SUCCESS, "TWI transfer failed: %d", err);
 
-	nrfx_twi_disable(&twi);
+  nrfx_twi_disable(&twi);
 }
