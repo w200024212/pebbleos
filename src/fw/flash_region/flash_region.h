pebble-dev/pebble-firmware

fw/flash_region: define obelix flash layout
Note the amount of flash is not the final one (this is based on
a Sifli devkit, where 16M are available). On this SoC, code is
executed from external flash as well.

Signed-off-by: Gerard Marull-Paretas <gerard@teslabs.com>

src/fw/flash_region/flash_region.h
@@ -43,6 +43,8 @@
 #include "flash_region_mt25q.h"
 #elif PLATFORM_SNOWY || PLATFORM_SPALDING
 #include "flash_region_s29vs.h"
+#elif PLATFORM_OBELIX
+#include "flash_region_sf32lb_int.h"
 #endif
 
 // NOTE: The following functions are deprecated! New code should use the

w200024212/pebbleos

src/fw/flash_region/flash_region.h
@@ -43,6 +43,8 @@
 #include "flash_region_mt25q.h"
 #elif PLATFORM_SNOWY || PLATFORM_SPALDING
 #include "flash_region_s29vs.h"
+#elif PLATFORM_OBELIX
+#include "flash_region_py25q128ha.h"
 #endif
 
 // NOTE: The following functions are deprecated! New code should use the