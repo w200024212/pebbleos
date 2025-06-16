5bd194246c9f0216b5a8cf966cd21a2ee004e3b3
Gerard Marull-Paretas <gerard@teslabs.com>
2025-06-16 14:50:48 +0200

    fw/comm/ble: set MTU change log level to INFO
    Signed-off-by: Gerard Marull-Paretas <gerard@teslabs.com>

--- src/fw/comm/ble/gatt.c
+++ src/fw/comm/ble/gatt.c
@@ -72,7 +72,7 @@ void bt_driver_cb_gatt_handle_mtu_update(const GattDeviceMtuUpdateEvent *event) 
       goto unlock;
     }
     
-    PBL_LOG(LOG_LEVEL_DEBUG, "Handle MTU change from %d to %d bytes",
+    PBL_LOG(LOG_LEVEL_INFO, "Handle MTU change from %d to %d bytes",
             connection->gatt_mtu, event->mtu);
     connection->gatt_mtu = event->mtu;
   }
