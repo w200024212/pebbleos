b4edca98a027e8c2d96900badfff56125a537b94
Gerard Marull-Paretas <gerard@teslabs.com>
2025-06-19 11:47:59 +0200

    fw/services/common/bluetooth: use 'Core XXXX' adv name for obelix
    Signed-off-by: Gerard Marull-Paretas <gerard@teslabs.com>

--- src/fw/services/common/bluetooth/local_id.c
+++ src/fw/services/common/bluetooth/local_id.c
@@ -44,7 +44,7 @@ static void prv_set_default_device_name(void) {
 #if (PLATFORM_SNOWY || PLATFORM_SPALDING || PLATFORM_ROBERT)
   const char *s_local_default_device_name_format = "Pebble Time %02X%02X";
   const char *s_local_default_le_device_name_format = "Pebble Time LE %02X%02X";
-#elif PLATFORM_ASTERIX
+#elif PLATFORM_ASTERIX || PLATFORM_OBELIX
   const char *s_local_default_device_name_format = "Core %02X%02X";
   const char *s_local_default_le_device_name_format = "Core %02X%02X";
 #else
