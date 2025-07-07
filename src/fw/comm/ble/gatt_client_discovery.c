fb2bc239065bd01e2d78c698d38b44c27cb475f7
Gerard Marull-Paretas <gerard@teslabs.com>
2025-07-07 17:02:25 +0200

    fw/comm/ble: do not change connection parameters if discovery failed
    It could well be that connection is no longer valid.
    
    Signed-off-by: Gerard Marull-Paretas <gerard@teslabs.com>

--- src/fw/comm/ble/gatt_client_discovery.c
+++ src/fw/comm/ble/gatt_client_discovery.c
@@ -397,11 +397,15 @@ bool bt_driver_cb_gatt_client_discovery_complete(GAPLEConnection *connection, BT
       // we get a BTErrnoOK which means we will get a completion event already
       finalize_discovery = (errno != BTErrnoOK);
     }
-    // Completion of service discovery implies we are about to have more BLE
-    // traffic (for example, ANCS notifications, PPoG communication). Keep the
-    // channel at a high throughput speed for a little bit longer to handle these bursts.
-    conn_mgr_set_ble_conn_response_time(connection, BtConsumerLeServiceDiscovery,
-                                        ResponseTimeMin, 10);
+
+    if (errno == BTErrnoOK) {
+      // Completion of service discovery implies we are about to have more BLE
+      // traffic (for example, ANCS notifications, PPoG communication). Keep the
+      // channel at a high throughput speed for a little bit longer to handle these bursts.
+      conn_mgr_set_ble_conn_response_time(connection, BtConsumerLeServiceDiscovery,
+                                          ResponseTimeMin, 10);
+    }
+
     if (finalize_discovery) {
       prv_finalize_discovery(connection, errno);
     }
