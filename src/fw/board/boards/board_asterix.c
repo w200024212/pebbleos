6bfd2581ece82c918ff02ec2fc6b8104e339c132
Joshua Wise <joshua@accelerated.tech>
2025-07-07 21:30:27 -0700

    fw/board/asterix: do not set outputs that are not in fact outputs on this board
    The board init code had GPIO settings for previous hardware that are out
    of date for asterix_evt3, including setting some pins as outputs that,
    at best, will be overridden, and at worst, could be a drive fight later.
    Remove those.
    
    Signed-off-by: Joshua Wise <joshua@accelerated.tech>

--- src/fw/board/boards/board_asterix.c
+++ src/fw/board/boards/board_asterix.c
@@ -201,13 +201,6 @@ IRQ_MAP_NRFX(RTC1, rtc_irq_handler);
 void board_early_init(void) {
   PBL_LOG(LOG_LEVEL_ERROR, "asterix early init");
 
-  /* shared SPI chip outputs */
-  nrf_gpio_cfg_output(15);
-  nrf_gpio_cfg_output(16);
-  nrf_gpio_pin_set(15);
-  
-  nrf_gpio_pin_set(16);
- 
   NRF_NVMC->ICACHECNF |= NVMC_ICACHECNF_CACHEEN_Msk;
 
   nrf_clock_lf_src_set(NRF_CLOCK, NRF_CLOCK_LFCLK_XTAL);
